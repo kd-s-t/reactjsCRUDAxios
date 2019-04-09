ca4342ca1813d1f43bed883996f3d9eb7aa3a473
Kenn Tinio <kendantinio@gmail.com>
2019-04-09 14:17:49 +0800

    first commit

--- README.md
+++ README.md
@@ -1,68 +1,32 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# Reactjs CRUD Axios   
 
-## Available Scripts
+# Required  
+[Run this API Python Flask](https://github.com/kenntinio/pythonFlaskAPICRUD)  
 
-In the project directory, you can run:
+# Operations  
 
-### `npm start`
+* Create  
+* Read    
+* Update    
+* Delete   
 
-Runs the app in the development mode.<br>
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+# Usage  
+```
+npm run
+```
+  
+# UI
+## Create  
+<img src="assets/img/create.png" width="30%">   
 
-The page will reload if you make edits.<br>
-You will also see any lint errors in the console.
+## Read  
+<img src="assets/img/read.png" width="30%">   
 
-### `npm test`
+## Update  
+<img src="assets/img/update.png" width="30%">   
 
-Launches the test runner in the interactive watch mode.<br>
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+## Delete  
+<img src="assets/img/delete.png" width="30%">   
 
-### `npm run build`
+[JSON Formatter](https://jsonformatter.curiousconcept.com/)  
 
-Builds the app for production to the `build` folder.<br>
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br>
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `npm run build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
--- src/App.js
+++ src/App.js
@@ -1,25 +1,234 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      name: '',
+      address: '',
+      users: [],
+      new_id: '',
+      new_name: '',
+      new_address: '',
+      create_json: [],
+      read_json: [],
+      update_json: [],
+      delete_json: [],
+    }
+
+    this.create = this.create.bind(this)
+    this.read = this.read.bind(this)
+    this.update = this.update.bind(this)
+    this.delete = this.delete.bind(this)
+
+  }
+
+  componentDidMount() {
+    this.read()
+  }
+
+  create(e) {
+    e.preventDefault()
+
+    var formData = new FormData()
+    formData.append('name', this.state.name);
+    formData.append('address', this.state.address);
+
+    axios.post('http://127.0.0.1:5000/users/create', formData).then(function (response) {
+      this.read()
+      this.setState(() => {
+        return {create_json: response.data}
+      })
+    }.bind(this)).catch(function (error) {
+      // handle error
+      console.log(error)
+    }).then(function () {
+      // always executed
+    });
+  }
+
+  read(e) {
+    axios.get('http://127.0.0.1:5000/users').then(function (response) {
+      this.setState(() => {
+        return {users: response.data.users}
+      })
+      this.setState(() => {
+        return {read_json: response.data}
+      })
+    }.bind(this)).catch(function (error) {
+      // handle error
+    }).then(function () {
+      // always executed
+    });
+  }
+
+  selectToUpdate(user) {
+    this.setState({new_id: user.id})
+    this.setState({new_name: user.name})
+    this.setState({new_address: user.address})
+  }
+
+  update(e) {
+    e.preventDefault()
+
+    var formData = new FormData()
+    formData.append('name', this.state.new_name);
+    formData.append('address', this.state.new_address);
+
+    axios.post('http://127.0.0.1:5000/users/'+this.state.new_id+'/update', formData).then(function (response) {
+      this.read()
+      this.setState(() => {
+        return {update_json: response.data}
+      })
+    }.bind(this)).catch(function (error) {
+      // handle error
+      console.log(error)
+    }).then(function () {
+      // always executed
+    });
+  }
+
+  delete(key, user_id) {
+    axios.get('http://127.0.0.1:5000/users/'+user_id+'/delete').then(function (response) {
+      this.state.users.splice(key, 1);
+      this.setState({users: this.state.users})
+      this.setState(() => {
+        return {delete_json: response.data}
+      })
+    }.bind(this)).catch(function (error) {
+      // handle error
+    }).then(function () {
+      // always executed
+    });
+  }
+
   render() {
+    
+    const { users, create_json, read_json, update_json, delete_json } = this.state
+
+    var readUsers = users.map((user, key) =>
+        <tr key={key}>
+          <th scope="row">{user.id}</th>
+          <td>{user.name}</td>
+          <td>{user.address}</td>
+          <td>{user.created}</td>
+          <td><button type="button" className="btn btn-danger" onClick={() => this.selectToUpdate(user)}>Update</button></td>
+        </tr>
+    );
+
+    var deleteUsers = users.map((user, key) =>
+        <tr key={key}>
+          <th scope="row">{user.id}</th>
+          <td>{user.name}</td>
+          <td>{user.address}</td>
+          <td>{user.created}</td>
+          <td><button type="button" className="btn btn-danger" onClick={() => this.delete(key, user.id)}>Delete</button></td>
+        </tr>
+    );
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="row" style={{textAlign: 'left'}}>
+          <div className="col-sm">
+            <h1>Create User</h1>
+          </div>
+          <div className="col-sm">
+            <h1>Read User</h1>
+          </div>
+          <div className="col-sm">
+            <h1>Update User</h1>
+          </div>
+          <div className="col-sm">
+            <h1>Delete User</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm">
+            <form onSubmit={this.create}>
+              <div className="form-group">
+                <label>Name</label>
+                <input type="text" value={this.state.name} 
+                  onChange={(e) => this.setState({name: e.target.value})}
+                  className="form-control" placeholder="Any name.."/>
+              </div>
+              <div className="form-group">
+                <label>Address</label>
+                <input type="text" value={this.state.address} 
+                  onChange={(e) => this.setState({address: e.target.value})}
+                  className="form-control" placeholder="Apartment, studio, or floor"/>
+              </div>
+              <button type="submit" className="btn btn-primary">Create</button>
+            </form> 
+          </div>
+          <div className="col-sm">   
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Name</th>
+                  <th>Address</th>
+                  <th>Created</th>
+                </tr>
+              </thead>
+              <tbody>
+                {readUsers}
+              </tbody>
+            </table>
+          </div>
+          <div className="col-sm">
+            <form onSubmit={this.update}>
+              <div className="form-group">
+                <label>ID</label> <br/>
+                <input type="text" value={this.state.new_id} className="form-control" disabled/>
+              </div>
+              <div className="form-group">
+                <label>Name</label>
+                <input type="text" value={this.state.new_name} 
+                  onChange={(e) => this.setState({new_name: e.target.value})}
+                  className="form-control" placeholder="Any name.."/>
+              </div>
+              <div className="form-group">
+                <label>Address</label>
+                <input type="text" value={this.state.new_address} 
+                  onChange={(e) => this.setState({new_address: e.target.value})}
+                  className="form-control" placeholder="Apartment, studio, or floor"/>
+              </div>
+              <button type="submit" className="btn btn-primary" >Update</button>
+            </form> 
+          </div>
+          <div className="col-sm">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Name</th>
+                  <th>Address</th>
+                  <th>Created</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {deleteUsers}
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm">
+          {JSON.stringify(create_json)}
+          </div>
+          <div className="col-sm">
+          {JSON.stringify(read_json)}
+          </div>
+          <div className="col-sm">
+          {JSON.stringify(update_json)}
+          </div>
+          <div className="col-sm">
+          {JSON.stringify(delete_json)}
+          </div>
+        </div>
       </div>
     );
   }
